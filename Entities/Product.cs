7889d0cea75f2a174421c18c1d22724e4d8b3c4f
AnilGoindi <114911637+AnilGoindi@users.noreply.github.com>
2022-10-05 09:54:16 +0100

    Fix connection string

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -9,6 +9,6 @@
         public string PictureUrl { get; set; }
         public string Type { get; set; }
         public string Brand { get; set; }
-        public int QuantityInStock { get; set; }
+        public int QuantityInStock { get; set;  }
     }
 }
