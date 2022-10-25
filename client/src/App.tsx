b19f8c1b3bcc632cea6c3c5a569940470d978aac
AnilGoindi <114911637+AnilGoindi@users.noreply.github.com>
2022-10-25 17:02:37 +0100

    App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return (
-    <div>
-          <h1>Re-Store</h1>
-          {console.log('hello')}
-    </div>
-  );
+    const [products, setProducts] = useState([
+        { name: 'product1', price: 100.00 },
+        { name: 'product2', price: 200.00 }
+    ]);
+
+    function addProduct() {
+        setProducts(prevState => [...prevState,
+            {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}])
+    }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/api/products')
+            .then(response => response.json())
+            .then(data => setProducts(data))
+    }, [])
+
+    return (
+        <div className='app'>
+            <h1>Re-Store</h1>
+            <>
+                <ul>
+                    {products.map((item, index) => (
+                        <li key={index}>{item.name} - {item.price}</li>
+                    ))}
+                </ul>
+                <button onClick={addProduct}>Add product</button>
+            </>
+        </div>
+    );
 }
 
 export default App;
