6637d3a05472caea84548ad9e4426f428c02f6a8
AnilGoindi <114911637+AnilGoindi@users.noreply.github.com>
2022-11-03 09:01:23 +0000

    Upto step 36

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,4 +1,4 @@
-﻿import { Avatar, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+﻿import { Grid, List } from "@mui/material";
 import { Product } from "../../models/product";
 import ProductCard from "./ProductCard";
 
@@ -7,13 +7,17 @@ interface Props {
 }
 
 export default function ProductList({products}: Props) {
-    return (
-        
-            <List>
-            {products.map(product => (
-                <ProductCard key={product.id} product={product} />
+    return (        
+        <Grid container spacing={4} >
+         <>
+                {products.map(product => (
+                    <Grid item xs={3} key={product.id}>
+                    <ProductCard product={product} />
+                </Grid>
             ))}
-            </List>
+         </>
+        </Grid>
+        
        
     )
 }
