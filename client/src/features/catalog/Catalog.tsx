c75466f68858a0aa6d17e97f9d9aaf92417380ff
AnilGoindi <114911637+AnilGoindi@users.noreply.github.com>
2022-11-04 08:54:44 +0000

    Step 38

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,18 +1,21 @@
-﻿import { Avatar, Button, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+﻿import { useEffect, useState } from "react";
 import { Product } from "../../models/product";
 import ProductList from "./ProductList";
 
 
-interface Props {
-    products: Product[];
-    addProduct: () => void;
-}
+export default function Catalog() {
 
-export default function Catalog({products, addProduct}: Props) {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/api/products')
+            .then(response => response.json())
+            .then(data => setProducts(data))
+    }, [])
+
     return (
         <>
             <ProductList products={products} />
-            <Button variant='contained' onClick={addProduct}>Add product</Button>
         </>
     )
 }
