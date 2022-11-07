1ba460195a623b2bc5dce9f6f552e4c801229aa1
AnilGoindi <114911637+AnilGoindi@users.noreply.github.com>
2022-11-07 11:14:27 +0000

    step 42 header.tsx

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -26,17 +26,29 @@ export default function Header({ darkMode, handleThemeChange }: Props) {
                     RE-STORE
                 </Typography>
                 <Switch checked={darkMode} onChange={handleThemeChange} />
-                <List>
+                <List sx={{display: 'flex'}}>
                     {midLinks.map(({ title, path }) => (
                         <ListItem
                             component={NavLink}
+                            to={path}
+                            key={path}
+                            sx={{color: 'inherit', typography: 'h6'}}
+                        >
+                            {title.toUpperCase()}
+                        </ListItem>                                           
+                    ))}
+                </List>
+                <List sx={{ display: 'flex' }}>
+                    {rightLinks.map(({ title, path }) => (
+                        <ListItem
+                            component={NavLink}
+                            to={path}
+                            key={path}
+                            sx={{ color: 'inherit', typography: 'h6' }}
                         >
                             {title.toUpperCase()}
                         </ListItem>
-                    
-                        
                     ))}
-
                 </List>
             </Toolbar>
         </AppBar>
