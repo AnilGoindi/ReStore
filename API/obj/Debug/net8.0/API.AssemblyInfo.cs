c7b31bfd52f7e1c7bd588d712ef9c7d0b24be5e7
AnilGoindi <114911637+AnilGoindi@users.noreply.github.com>
2024-11-25 15:49:38 +0000

    Step 37

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b7fc6c5eab7c070f2715c2e9109f33087920ddc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6432064bec897738e36b27ad06d29b95ed2a897")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
