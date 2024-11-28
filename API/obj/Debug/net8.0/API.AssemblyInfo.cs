a61874d686e8a8087093e92da738e99aafdc9b19
AnilGoindi <114911637+AnilGoindi@users.noreply.github.com>
2024-11-28 11:19:33 +0000

    Step 47 setup error controller

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+719d410168e286da4219f797f8dc4b2a941a05ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f34125e14e410ddd1fa08442598fcdf62223c3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
