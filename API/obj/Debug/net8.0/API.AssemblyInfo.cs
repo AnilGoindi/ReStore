f8cf1617958db6ec594e45ecc0fd93ff1f687e96
AnilGoindi <114911637+AnilGoindi@users.noreply.github.com>
2024-12-02 14:24:00 +0000

    Step 54 Creating server error component (nb error in step 53 still with non display of 2 error strings)

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717bc8e12728fd2074b7c9e0e2c2f2f447ecf05f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65c24f4c06d1f557132d46e19972365f8c43ee92")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
