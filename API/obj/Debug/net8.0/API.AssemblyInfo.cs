296e0d329c66adddefd291f3c813dd58c3980651
AnilGoindi <114911637+AnilGoindi@users.noreply.github.com>
2024-12-02 14:57:57 +0000

    Step 55 Creating a not found component

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65c24f4c06d1f557132d46e19972365f8c43ee92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8cf1617958db6ec594e45ecc0fd93ff1f687e96")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
 import agent from "../../app/api/agent";
+import NotFound from "../../errors/NotFound";
 
 
 export default function ProductDetails() {
@@ -20,7 +21,7 @@ useEffect(() => {
 
 if (loading) return <h3>Loading...</h3>
 
-if (!product) return <h3>Product not found</h3>
+if (!product) return <NotFound />
 
     return(
         <Grid container spacing={6}>
