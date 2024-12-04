b8f6b95da596f05366819192e3b1049a1e689f87
AnilGoindi <114911637+AnilGoindi@users.noreply.github.com>
2024-12-04 13:04:53 +0000

    Step 62 Creating Basket entity

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86deb968aede626801d9ad553711daf7316ade5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9610d6f67ef2eebe3fcf6535b24dcb3251012198")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
