7920079ea3d9a635f6201894c00b51b5b5eb2b99
AnilGoindi <114911637+AnilGoindi@users.noreply.github.com>
2024-12-05 13:15:07 +0000

    step 67 Shaping the data to return

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8f6b95da596f05366819192e3b1049a1e689f87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eb1c3912ac295e36beab2ccf09eeb29bb38c001")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
